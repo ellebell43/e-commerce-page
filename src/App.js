d615501d3b3adf59c892b79af5d1cfe2b1c97264
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-08-13 17:30:08 -0700

    Mobile header
    - Created the mobile header
    - - includes nav menu button, logo, cart button, and avatar picture.
    - - All buttons are currently non-functional

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import "./App.css";
+import { motion } from "framer-motion";
+// images import
+import menuIcon from "./images/icon-menu.svg";
+import logo from "./images/logo.svg";
+import cartIcon from "./images/icon-cart.svg";
+import avatar from "./images/image-avatar.png";
 
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <div className="app">
+      <header>
+        <div className="navLogoContainer">
+          <motion.button whileHover={{ scale: 1.1 }} className="menuBtn">
+            <img src={menuIcon} alt="menu" />
+          </motion.button>
+          <img src={logo} alt="sneakers logo" className="logo" />
+        </div>
+        <div className="profileCartContainer">
+          <motion.button whileHover={{ scale: 1.1 }}>
+            <img src={cartIcon} alt="your cart" />
+          </motion.button>
+          <img src={avatar} alt="your avatar" className="avatar" />
+        </div>
+      </header>
+    </div>
   )
 }
 
