718a59cee9ccb54b94735a5f06599c13e2af4e67
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-08-13 17:08:30 -0700

    Additional setup
    - Setup SASS file for styling
    - Setup a couple component files (Cart.js and Nav.js

---
+++ src/components/Cart.js
@@ -0,0 +1,9 @@
+const Cart = () => {
+  return (
+    <div>
+      <p>I'm the cart :)</p>
+    </div>
+  )
+}
+
+export default Cart;
---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+const Nav = () => {
+  return (
+    <nav>
+      <p>Navigator!</p>
+    </nav>
+  )
+}
+
+export default Nav;
