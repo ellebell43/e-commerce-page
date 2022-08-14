db6eee37fff3b4c62c56fb16e2ad9c67d5e1ab36
Aaron Rutherford <rutherfordaaron1@gmail.com>
2022-08-13 21:47:39 -0700

    Created images section
    - Added in product images
    - Can select image to view via button (mobile) or thumbnails (desktop)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
 // component import
 import Nav from "./components/Nav";
+import ProductImgs from "./components/ProductImgs";
 // images import
 import menuIcon from "./images/icon-menu.svg";
 import logo from "./images/logo.svg";
@@ -13,7 +14,8 @@ import avatar from "./images/image-avatar.png";
 const App = () => {
   let [navVisibility, setNavVisibility] = useState(false);
   let [cartVisibility, setCartVisibility] = useState(false);
-  const [width, setWidth] = useState(window.innerWidth);
+  let [imgIndex, setImgIndex] = useState(0);
+  let [width, setWidth] = useState(window.innerWidth);
   const updateDimensions = () => {
     setWidth(window.innerWidth);
   }
@@ -50,6 +52,10 @@ const App = () => {
           <img src={avatar} alt="your avatar" className="avatar" />
         </div>
       </header>
+      <main>
+        <ProductImgs imgIndex={imgIndex} setImgIndex={setImgIndex} />
+        <h1>Testttttt... o_o</h1>
+      </main>
       <div
         className={`lightbox ${navVisibility || cartVisibility ? "" : "hidden"}`}
         onClick={lightboxClick}
---
+++ src/components/ProductImgs.js
@@ -0,0 +1,68 @@
+import { motion } from "framer-motion";
+
+import nextIcon from "../images/icon-next.svg";
+import prevIcon from "../images/icon-previous.svg";
+
+// product images
+// full images
+import img0 from "../images/image-product-1.jpg";
+import img1 from "../images/image-product-2.jpg";
+import img2 from "../images/image-product-3.jpg";
+import img3 from "../images/image-product-4.jpg";
+// thumbnails
+import thumb0 from "../images/image-product-1-thumbnail.jpg";
+import thumb1 from "../images/image-product-2-thumbnail.jpg";
+import thumb2 from "../images/image-product-3-thumbnail.jpg";
+import thumb3 from "../images/image-product-4-thumbnail.jpg";
+// img arrays
+const imgArr = [img0, img1, img2, img3];
+const thumbArr = [thumb0, thumb1, thumb2, thumb3];
+
+const ProductImgs = (props) => {
+  const changeImg = (num) => {
+    let index = props.imgIndex + num;
+    if (index > imgArr.length - 1) {
+      index = 0;
+    } else if (index === -1) {
+      index = imgArr.length - 1;
+    }
+
+    props.setImgIndex(index);
+  }
+
+  const thumbnailClick = (i) => {
+    props.setImgIndex(i);
+  }
+
+  return (
+    <div className="imgSection">
+      <div className="mainImg">
+        <div className="imgBtnContainer">
+          <motion.button whileTap={{ scale: 1.2 }} onClick={() => { changeImg(-1) }} className="mobileElement">
+            <img src={prevIcon} alt="" />
+          </motion.button>
+          <motion.button whileTap={{ scale: 1.2 }} onClick={() => { changeImg(1) }} className="mobileElement">
+            <img src={nextIcon} alt="" />
+          </motion.button>
+        </div>
+        <img src={imgArr[props.imgIndex]} alt="image of shoes" />
+      </div>
+      <div className="thumbnails desktopElement">
+        {thumbArr.map((el, i) => {
+          let activeClass = props.imgIndex === i ? "active" : "";
+          return (
+            <motion.img
+              whileTap={{ scale: 1.1 }}
+              key={i}
+              className={activeClass}
+              src={thumbArr[i]}
+              alt="product thumbnail"
+              onClick={() => { thumbnailClick(i) }}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+export default ProductImgs;
